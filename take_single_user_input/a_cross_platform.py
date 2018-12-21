ff9507fc49dbe933feeaacc9acd7f15bf34faecf
Mohd Hafizuddin Kamilin <34746052+hafiz-kamilin@users.noreply.github.com>
2018-12-21 21:47:17 +0900

    fix typo

--- take_single_user_input/a_cross_platform.py
+++ take_single_user_input/a_cross_platform.py
@@ -3,38 +3,53 @@
 
 # swap between unix-like or windows getch depend on user platform
 class _Getch:
-    """Gets a single character from standard input.  Does not echo to the
-screen."""
+
+    # gets a single character from standard input, does not echo to the screen
     def __init__(self):
+
         try:
+
             self.impl = _GetchWindows()
+
         except ImportError:
+
             self.impl = _GetchUnix()
 
     def __call__(self): return self.impl()
 
 # class for unix-like getch
 class _GetchUnix:
+
     def __init__(self):
+
         import tty, sys
 
     def __call__(self):
+
         import sys, tty, termios
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
+
         try:
+
             tty.setraw(sys.stdin.fileno())
             ch = sys.stdin.read(1)
+
         finally:
+
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
         return ch
 
 # class for windows getch
 class _GetchWindows:
+
     def __init__(self):
+
         import msvcrt
 
     def __call__(self):
+        
         import msvcrt
         return msvcrt.getch()
 
